697c8a962cb454a1aed687a6b1ca84162aaf8bb9
PutterChez <44222382+PutterChez@users.noreply.github.com>
2020-09-14 15:13:04 +0700

    camera listener

--- cameraListener.js
+++ cameraListener.js
@@ -1,13 +1,6 @@
-AFRAME.registerComponent('trigger-on-look-behind', {
-  schema: {type: 'string'},
-
-  init: function () {
-    var eventName = this.data;
-    this.el.addEventListener('componentchanged', function (evt) {
-      if (evt.name !== 'rotation') { return; }
+document.querySelector('[camera]').addEventListener('componentchanged', function (evt) {
+  if (evt.name !== 'rotation') { return; }
       if (evt.newData.y < 180) {
         this.emit(eventName);
       }
-    });
-  }
 });

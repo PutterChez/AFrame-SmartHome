08e688f616bea102c0fb2670c426246f53fc0795
PutterChez <44222382+PutterChez@users.noreply.github.com>
2020-09-23 15:31:08 +0700

    toggle

--- fanOff.js
+++ fanOff.js
@@ -1,5 +1,10 @@
 function turnOffFan () {
 	console.log('turn off');
 	var fan = document.getElementById("fan");
-	fan.setAttribute("animation","property: rotation; to: 0 360 0; loop: false; dur: 800;");
+	var status = fan.getAttribute("animation");
+
+	if(status == "property: rotation; to: 0 360 0; loop: true; dur: 800;")
+		fan.setAttribute("animation","property: rotation; to: 0 360 0; loop: false; dur: 800;");
+	else
+		fan.setAttribute("animation","property: rotation; to: 0 360 0; loop: true; dur: 800;");
 }	

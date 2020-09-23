85b98e1bd50987efa8f913367df55c0660f1d4a6
PutterChez <44222382+PutterChez@users.noreply.github.com>
2020-09-23 15:10:24 +0700

    model fix

--- fanOff.js
+++ fanOff.js
@@ -1,5 +1,5 @@
 function turnOffFan () {
 	console.log('turn off');
 	var fan = document.getElementById("fan");
-	fan.setAttribute("animation","");
+	fan.setAttribute("animation","property: rotation; to: 0 360 0; loop: false; dur: 800;");
 }	
